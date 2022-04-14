4de109d42ac9d5fba9f1091772b5c02f87500816
Eugene A. Kuchin <e.kuchin@gmail.com>
2022-04-14 20:54:49 +0500

    router 01 simple route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/cats">Cats</Link>
+            </li>
+            <li>
+              <Link to="/dogs">Dogs</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/cats" element={<Cats />}/>
+          <Route path="/dogs" element={<Dogs />}/>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Это домашняя страница</h2>;
+}
+
+function Cats() {
+  return <h2>Это список котиков</h2>;
+}
+
+function Dogs() {
+  return <h2>Это список песиков</h2>;
+}
