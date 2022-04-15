78eb3ae7fa6466b9a2e5be5627356f5e1ecf6b94
Eugene A. Kuchin <e.kuchin@gmail.com>
2022-04-15 07:33:01 +0500

    router-nested v1.0

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {
   BrowserRouter,
   Routes,
   Route,
+  Navigate,
+  useLocation,
   Link
 } from "react-router-dom";
 import routeData from "./data/routeData.js"
@@ -18,8 +20,8 @@ export default function App() {
   })
 
   const infoArray=routeData.map(doc =>{
-    return(
-      <Route key={doc.id} path={"/"+doc.id} element={<Info doc={doc} />}/>
+     return(
+      <Route key={doc.id} path={"/"+doc.id+"/*"} element={<Info doc={doc}/>}/>
     )
   })
   
@@ -33,16 +35,61 @@ export default function App() {
         </nav>
 
         <Routes>
+          <Route exact path="/" element={<Home/>}/>
           {infoArray}
+          <Route path="/contract" element={<Navigate replace to="/dog/abon"/>}/>
+          <Route path="*" element={<NoMatch/>}/>
         </Routes>
+
       </div>
     </BrowserRouter>
   );
 }
 
+function Home() {
+  return <h2>Это домашняя страница</h2>;
+}
+
+function NoMatch() {
+  return <h2>Это 404 страница</h2>;
+}
+
+
 function Info(props) {
+  
+  const nestedLinksArray=props.doc.types.map(type=>{
+    return(
+      <li key={type.id}><Link to={props.path+"/"+type.id}>{type.title}</Link></li>
+    )
+    })
+
+  const nestedInfoArray= props.doc.types.map(type =>{
+    return(   
+      
+      <Route key={type.id} path={props.path+"/"+type.id} element={<DetailedInfo type={type}/>}/>
+        
+    )
+  })
+  console.log(props.path)
+  console.log(nestedInfoArray)
   return (
-    <h2>
-      {props.doc.description}
-    </h2>)
+    <div>
+      <h2>{props.doc.description}</h2>
+      <p>Путь: {props.path}</p>
+      <ul>
+        {nestedLinksArray}
+      </ul>
+    
+      <Routes>
+        {//<Route exact path={props.path} element={<h3>Please select a type.</h3>}/>}
+}
+        {nestedInfoArray}       
+      </Routes>
+    </div>
+    
+    )
+}
+
+function DetailedInfo(props) {
+  return <h2>{props.type.description}</h2>;
 }
